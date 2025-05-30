962480c27ec698a00fa618785821ac85e39cb427
flatout <cindy.lewis@carculture.com>
2025-05-30 16:02:04 -0400

    Switch homepage logo to SVG for perfect sharpness

--- social/neynar_v2/src/app/page.tsx
+++ social/neynar_v2/src/app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm">
         <div style={{ display: "flex", justifyContent: "center", marginBottom: "2rem" }}>
           <Image
-            src="/logo-black.png"
+            src="/logo-black.svg"
             alt="CarCulture Logo"
             width={120}
             height={40}
