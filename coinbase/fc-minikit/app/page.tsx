49adb5ef8047ec11eadab642b03553a7b724a8eb
flatout <cindy.lewis@carculture.com>
2025-07-09 14:02:36 -0400

    Revert to @farcaster/frame-sdk for Vercel compatibility (miniapp-sdk not on npm)

--- coinbase/fc-minikit/app/page.tsx
+++ coinbase/fc-minikit/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useCallback } from "react";
-import { sdk } from '@farcaster/miniapp-sdk';
+import { sdk } from '@farcaster/frame-sdk';
 import Image from "next/image";
 import HeroWithStart from "./components/HeroWithShare";
 
